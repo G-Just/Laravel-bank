2cb843d11634545b58b8f4ed3f9754c52a06b443
G-Just <gjustas00@gmail.com>
2024-01-29 15:19:42 +0200

    Adding a favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Laravel Bank</title>
 
     <!-- Scripts -->
     @vite('resources/css/app.css')
