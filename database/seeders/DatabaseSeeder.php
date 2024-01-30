f394e44a87e6a3488c79c21d9216b862e9bb2e9e
G-Just <gjustas00@gmail.com>
2024-01-30 19:45:31 +0200

    Adding seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +16,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $faker = Faker::create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        foreach (range(1, 20) as $client) {
+            DB::table('clients')->insert([
+                'firstName' => $faker->firstName(),
+                'lastName' => $faker->lastName(),
+                'personalCode' => $faker->regexify('[0-9]{11}')
+            ]);
+        }
+
+        foreach (range(1, 100) as $accounts) {
+            DB::table('accounts')->insert([
+                'IBAN' => $faker->regexify('LT0099999[0-9]{10}'),
+                'balance' => $faker->numberBetween(0, 500000),
+                'client_id' => $faker->numberBetween(1, 20)
+            ]);
+        }
+
+        DB::table('users')->insert([
+            'name' => 'IS1ca7122a5fe82d',
+            'email' => '98f87fd651@gmail.com',
+            'password' => '$2y$12$ut5GSluYsR3gSLYhDYhX2.lj.vubcj/RzVrXg5QPVbzDbAF3GI5Km'
+        ]);
     }
 }
